781722d3d906e39c1f1d68be0b077fa10c786ffb
acjoker8818 <112934795+acjoker8818@users.noreply.github.com>
2023-06-03 07:55:30 -0500

    Add files via upload

--- resources/AcjokerScript/gizmoUtils.lua
+++ resources/AcjokerScript/gizmoUtils.lua
@@ -230,20 +230,20 @@ function ray_box_intersection(ray_origin, ray_dir, pos, rot, offset_min, offset_
 end
 
 enum LOSFlags begin
-    SCRIPT_INCLUDE_MOVER		= 1,
-    SCRIPT_INCLUDE_VEHICLE		= 2,
-    SCRIPT_INCLUDE_PED			= 4,
-    SCRIPT_INCLUDE_RAGDOLL		= 8,
-    SCRIPT_INCLUDE_OBJECT		= 16,
-    SCRIPT_INCLUDE_PICKUP		= 32,
-    SCRIPT_INCLUDE_GLASS		= 64,
-    SCRIPT_INCLUDE_RIVER		= 128,
-    SCRIPT_INCLUDE_FOLIAGE		= 256,
-    SCRIPT_INCLUDE_ALL			= 511
+    SCRIPT_INCLUDE_MOVER        = 1,
+    SCRIPT_INCLUDE_VEHICLE      = 2,
+    SCRIPT_INCLUDE_PED          = 4,
+    SCRIPT_INCLUDE_RAGDOLL      = 8,
+    SCRIPT_INCLUDE_OBJECT       = 16,
+    SCRIPT_INCLUDE_PICKUP       = 32,
+    SCRIPT_INCLUDE_GLASS        = 64,
+    SCRIPT_INCLUDE_RIVER        = 128,
+    SCRIPT_INCLUDE_FOLIAGE      = 256,
+    SCRIPT_INCLUDE_ALL          = 511
 end
 
 
-local SCRIPT_SHAPETEST_OPTION_IGNORE_NO_COLLISION	    = 4,
+local SCRIPT_SHAPETEST_OPTION_IGNORE_NO_COLLISION       = 4,
 
 local probe_start_pos_out = memory.alloc()
 local probe_end_pos_out = memory.alloc()
